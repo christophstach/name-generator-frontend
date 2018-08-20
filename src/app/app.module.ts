4c23cb58cf8753a3fa58a576ae8244c0a489caf3
Christoph Stach <christoph.stach@gmail.com>
2018-08-21 00:44:41 +0200

    better seo features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
-import { TransferHttpCacheModule } from '@nguniversal/common';
 
 @NgModule({
   declarations: [
@@ -17,9 +15,7 @@ import { TransferHttpCacheModule } from '@nguniversal/common';
     AppRoutingModule,
     CoreModule,
     BrowserAnimationsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    TransferHttpCacheModule
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [ AppComponent ]
