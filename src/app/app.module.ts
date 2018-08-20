2f39ec9cd6efbe359d6311061432db0ab037f4c6
Christoph Stach <christoph.stach@gmail.com>
2018-08-21 01:10:27 +0200

    added ssr again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { TransferHttpCacheModule } from '@nguniversal/common';
 import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -15,7 +17,9 @@ import { CoreModule } from './core/core.module';
     AppRoutingModule,
     CoreModule,
     BrowserAnimationsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    TransferHttpCacheModule
   ],
   providers: [],
   bootstrap: [ AppComponent ]
