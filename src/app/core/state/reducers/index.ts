ef1684f1e286fb9e86a4c7c5ad106f5d27d0f33c
Christoph Stach <christoph.stach@gmail.com>
2018-08-22 10:11:19 +0200

    removed apollo ngx store

--- src/app/core/state/reducers/index.ts
+++ src/app/core/state/reducers/index.ts
@@ -1,16 +1,13 @@
 import { ActionReducerMap, MetaReducer } from '@ngrx/store';
-import { apolloReducer, CacheState, } from 'apollo-angular-cache-ngrx';
 import { environment } from '../../../../environments/environment';
 import { GeneratorSettingsInterface } from '../../../shared/interfaces/generator-settings.interface';
 import { generatorSettingsReducer } from './generator-settings.reducer';
 
 export interface Root {
-  apollo: CacheState;
   generatorSettings: GeneratorSettingsInterface;
 }
 
 export const reducers: ActionReducerMap<Root> = {
-  apollo: apolloReducer,
   generatorSettings: generatorSettingsReducer
 };
 
--- src/app/core/state/state.module.ts
+++ src/app/core/state/state.module.ts
@@ -1,25 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
-import { metaReducers, reducers } from './reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../../../environments/environment';
-import { EffectsModule } from '@ngrx/effects';
 import { RootEffects } from './effects/root.effects';
-import { NgrxCache, NgrxCacheModule } from 'apollo-angular-cache-ngrx';
+import { metaReducers, reducers } from './reducers';
 
 @NgModule({
   imports: [
     CommonModule,
     StoreModule.forRoot(reducers, { metaReducers }),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
-    EffectsModule.forRoot([ RootEffects ]),
-    NgrxCacheModule
+    EffectsModule.forRoot([ RootEffects ])
   ],
   declarations: []
 })
 export class StateModule {
-  constructor(ngrxCache: NgrxCache) {
-    const cache = ngrxCache.create({});
-  }
 }
