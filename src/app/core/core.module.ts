ae3b8e77d49e955ae787cb6e6bebd249579164db
Christoph Stach <christoph.stach@gmail.com>
2018-09-06 15:54:50 +0200

    removed state module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { LayoutModule } from './layout/layout.module';
-import { StateModule } from './state/state.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    LayoutModule,
-    StateModule
+    LayoutModule
   ],
   declarations: [],
-  exports: [ LayoutModule ]
+  exports: [LayoutModule]
 })
 export class CoreModule {
 }
